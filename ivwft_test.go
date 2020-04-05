f187dcfeec6a543b07f0a66e985fbbd0e4629829
Louis Chan <louischan0325@gmail.com>
2020-04-05 15:11:44 +0800

    Add more ivwft tests

--- ivwft_test.go
+++ ivwft_test.go
@@ -11,19 +11,19 @@ func TestIVWFT(t_ *testing.T) {
 			t_.Errorf("err: %v\n", err)
 		} else {
 			if i != ii {
-				t_.Errorf("%d != %d\n", i, ii)
+				t_.Errorf("%#v: %d != %d\n", input, i, ii)
 			}
 			if v != vv {
-				t_.Errorf("%d != %d\n", v, vv)
+				t_.Errorf("%#v: %d != %d\n", input, v, vv)
 			}
 			if w != ww {
-				t_.Errorf("%d != %d\n", w, ww)
+				t_.Errorf("%#v: %d != %d\n", input, w, ww)
 			}
 			if f != ff {
-				t_.Errorf("%d != %d\n", f, ff)
+				t_.Errorf("%#v: %d != %d\n", input, f, ff)
 			}
 			if t != tt {
-				t_.Errorf("%d != %d\n", t, tt)
+				t_.Errorf("%#v: %d != %d\n", input, t, tt)
 			}
 		}
 	}
@@ -32,21 +32,36 @@ func TestIVWFT(t_ *testing.T) {
 	test(1.0, 1, 0, 0, 0, 0)
 	test("1", 1, 0, 0, 0, 0)
 	test("01", 1, 0, 0, 0, 0)
+	test("-1", 1, 0, 0, 0, 0)
+	test("-01", 1, 0, 0, 0, 0)
 
 	test("1.0", 1, 1, 0, 0, 0)
+	test("-1.0", 1, 1, 0, 0, 0)
 	test("1.00", 1, 2, 0, 0, 0)
+	test("-1.00", 1, 2, 0, 0, 0)
 
 	test(1.3, 1, 1, 1, 3, 3)
+	test(-1.3, 1, 1, 1, 3, 3)
 	test("1.3", 1, 1, 1, 3, 3)
+	test("-1.3", 1, 1, 1, 3, 3)
 
 	test("1.30", 1, 2, 1, 30, 3)
+	test("-1.30", 1, 2, 1, 30, 3)
 
 	test(1.03, 1, 2, 2, 3, 3)
+	test(-1.03, 1, 2, 2, 3, 3)
 	test("1.03", 1, 2, 2, 3, 3)
+	test("-1.03", 1, 2, 2, 3, 3)
 
 	test(1.23, 1, 2, 2, 23, 23)
+	test(-1.23, 1, 2, 2, 23, 23)
 	test("1.230", 1, 3, 2, 230, 23)
+	test("-1.230", 1, 3, 2, 230, 23)
 
 	test(1234, 1234, 0, 0, 0, 0)
+	test(-1234, 1234, 0, 0, 0, 0)
 	test(1234.0, 1234, 0, 0, 0, 0)
+	test(-1234.0, 1234, 0, 0, 0, 0)
+	test("1234.0", 1234, 1, 0, 0, 0)
+	test("-1234.0", 1234, 1, 0, 0, 0)
 }
