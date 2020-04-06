d4f63a0b7f07df2ac9c74ebc9e440d3b4fd90ca7
Louis Chan <louischan@oursky.com>
2020-04-06 16:20:02 +0800

    Add FormatPositional

--- format.go
+++ format.go
@@ -38,13 +38,18 @@ func sliceArgsToNamedArgs(args []interface{}) (out map[string]interface{}) {
 	return
 }
 
+type argumentMinusOffset struct {
+	Name  string
+	Value interface{}
+}
+
 type formatter struct {
 	Buf  *strings.Builder
 	Tag  language.Tag
 	Args map[string]interface{}
 }
 
-func (f *formatter) Format(nodes []Node, pluralArgument *Argument) (err error) {
+func (f *formatter) Format(nodes []Node, argMinusOffset *argumentMinusOffset) (err error) {
 	for _, inode := range nodes {
 		switch node := inode.(type) {
 		case TextNode:
@@ -53,6 +58,10 @@ func (f *formatter) Format(nodes []Node, pluralArgument *Argument) (err error) {
 			err = f.FormatNoneArgNode(node)
 		case SelectArgNode:
 			err = f.FormatSelectArgNode(node)
+		case PluralArgNode:
+			err = f.FormatPluralArgNode(node)
+		case PoundNode:
+			err = f.FormatPoundNode(argMinusOffset)
 		}
 		if err != nil {
 			return
@@ -118,15 +127,12 @@ func (f *formatter) FormatTextNode(node TextNode) (err error) {
 }
 
 func (f *formatter) FormatNoneArgNode(node NoneArgNode) (err error) {
-	var argName string
-	var argValue interface{}
-	argName, argValue, err = f.ResolveArgument(node.Arg)
+	argName, argValue, err := f.ResolveArgument(node.Arg)
 	if err != nil {
 		return
 	}
 
-	var stringValue string
-	stringValue, err = f.FormatValue(argName, argValue)
+	stringValue, err := f.FormatValue(argName, argValue)
 	if err != nil {
 		return
 	}
@@ -136,15 +142,12 @@ func (f *formatter) FormatNoneArgNode(node NoneArgNode) (err error) {
 }
 
 func (f *formatter) FormatSelectArgNode(node SelectArgNode) (err error) {
-	var argName string
-	var argValue interface{}
-	argName, argValue, err = f.ResolveArgument(node.Arg)
+	argName, argValue, err := f.ResolveArgument(node.Arg)
 	if err != nil {
 		return
 	}
 
-	var stringValue string
-	stringValue, err = f.FormatValue(argName, argValue)
+	stringValue, err := f.FormatValue(argName, argValue)
 	if err != nil {
 		return
 	}
@@ -174,5 +177,167 @@ func (f *formatter) FormatSelectArgNode(node SelectArgNode) (err error) {
 		f.Format(otherClause.Nodes, nil)
 	}
 
+	return
+}
+
+func (f *formatter) FormatPluralArgNode(node PluralArgNode) (err error) {
+	argName, argValue, err := f.ResolveArgument(node.Arg)
+	if err != nil {
+		return
+	}
+
+	offsetValue, err := f.OffsetValue(argName, argValue, node.Offset)
+	if err != nil {
+		return
+	}
+
+	argumentMinusOffset := &argumentMinusOffset{
+		Name:  argName,
+		Value: offsetValue,
+	}
+
+	var otherClause *PluralClause
+	var done bool
+
+	for _, clause := range node.Clauses {
+		if clause.Keyword == "other" {
+			c := clause
+			otherClause = &c
+		}
+
+		if clause.Keyword == "" {
+			var match bool
+			match, err = f.MatchExplicitValue(argName, argValue, clause.ExplicitValue)
+			if err != nil {
+				return
+			}
+			if match {
+				done = true
+				f.Format(clause.Nodes, argumentMinusOffset)
+				break
+			}
+		}
+	}
+
+	if done {
+		return
+	}
+
+	pluralFunc := Cardinal
+	if node.Kind == "selectordinal" {
+		pluralFunc = Ordinal
+	}
+	pluralForm, err := pluralFunc(f.Tag, offsetValue)
+	if err != nil {
+		return
+	}
+
+	for _, clause := range node.Clauses {
+		if clause.Keyword == pluralForm {
+			done = true
+			f.Format(clause.Nodes, argumentMinusOffset)
+			break
+		}
+	}
+
+	if !done {
+		if otherClause == nil {
+			err = fmt.Errorf("missing plural other clause: %v", argName)
+			return
+		}
+		f.Format(otherClause.Nodes, argumentMinusOffset)
+	}
+
+	return
+}
+
+func (f *formatter) OffsetValue(argName string, value interface{}, offset int) (offsetValue interface{}, err error) {
+	switch v := value.(type) {
+	case int8:
+		offsetValue = int8(int64(v) - int64(offset))
+	case int16:
+		offsetValue = int16(int64(v) - int64(offset))
+	case int32:
+		offsetValue = int32(int64(v) - int64(offset))
+	case int64:
+		offsetValue = int64(int64(v) - int64(offset))
+	case int:
+		offsetValue = int(int64(v) - int64(offset))
+	case uint8:
+		offsetValue = uint8(int64(v) - int64(offset))
+	case uint16:
+		offsetValue = uint16(int64(v) - int64(offset))
+	case uint32:
+		offsetValue = uint32(int64(v) - int64(offset))
+	case uint64:
+		offsetValue = uint64(int64(v) - int64(offset))
+	case uint:
+		offsetValue = uint(int64(v) - int64(offset))
+	case float32:
+		offsetValue = float32(float32(v) - float32(offset))
+	case float64:
+		offsetValue = float64(float64(v) - float64(offset))
+	case string:
+		var f64 float64
+		f64, err = strconv.ParseFloat(v, 64)
+		if err != nil {
+			return
+		}
+		offsetValue = strconv.FormatFloat(f64-float64(offset), 'f', -1, 64)
+	default:
+		err = fmt.Errorf("expected numeric type: %v %T", argName, value)
+	}
+	return
+}
+
+func (f *formatter) MatchExplicitValue(argName string, value interface{}, explicitValue int) (match bool, err error) {
+	switch v := value.(type) {
+	case int8:
+		match = int64(v) == int64(explicitValue)
+	case int16:
+		match = int64(v) == int64(explicitValue)
+	case int32:
+		match = int64(v) == int64(explicitValue)
+	case int64:
+		match = int64(v) == int64(explicitValue)
+	case int:
+		match = v == explicitValue
+	case uint8:
+		match = int64(v) == int64(explicitValue)
+	case uint16:
+		match = int64(v) == int64(explicitValue)
+	case uint32:
+		match = int64(v) == int64(explicitValue)
+	case uint64:
+		match = int64(v) == int64(explicitValue)
+	case uint:
+		match = int64(v) == int64(explicitValue)
+	case float32:
+		match = float32(v) == float32(explicitValue)
+	case float64:
+		match = float64(v) == float64(explicitValue)
+	case string:
+		var f64 float64
+		f64, err = strconv.ParseFloat(v, 64)
+		if err != nil {
+			return
+		}
+		match = f64 == float64(explicitValue)
+	default:
+		err = fmt.Errorf("expected numeric type: %v %T", argName, value)
+	}
+	return
+}
+
+func (f *formatter) FormatPoundNode(argumentMinusOffset *argumentMinusOffset) (err error) {
+	if argumentMinusOffset == nil {
+		err = fmt.Errorf("lexer emitted pound token incorrectly")
+		return
+	}
+	out, err := f.FormatValue(argumentMinusOffset.Name, argumentMinusOffset.Value)
+	if err != nil {
+		return
+	}
+	f.Buf.WriteString(out)
 	return
 }
--- format_test.go
+++ format_test.go
@@ -66,4 +66,89 @@ func TestFormatPositional(t *testing.T) {
 		"He jumps over the lazy dog",
 		"male",
 		"the lazy dog")
+
+	// Simple plural
+	test(`{0, plural, one{# cat} other{# cats}}`, "1 cat", 1)
+	test(`{0, plural, one{# cat} other{# cats}}`, "2 cats", 2)
+
+	// plural with explicit value
+	test(`{0, plural, =0{no cats} one{# cat} other{# cats}}`, "no cats", 0)
+
+	// plural with offset
+	test(`{0, plural, offset:1
+		=1{Kitty and no other cats}
+		one{Kitty and 1 other cat}
+		other{Kitty and # other cats}}`,
+		"Kitty and no other cats",
+		1)
+	test(`{0, plural, offset:1
+		=1{Kitty and no other cats}
+		one{Kitty and 1 other cat}
+		other{Kitty and # other cats}}`,
+		"Kitty and 1 other cat",
+		2)
+	test(`{0, plural, offset:1
+		=1{Kitty and no other cats}
+		one{Kitty and 1 other cat}
+		other{Kitty and # other cats}}`,
+		"Kitty and 2 other cats",
+		3)
+	test(`{0, plural, offset:1
+		=1{Kitty and no other cats}
+		one{Kitty and # other cat}
+		other{Kitty and # other cats}}`,
+		"Kitty and -1 other cat",
+		0)
+	test(`{0, plural, offset:1
+		=0{No Kitty}
+		=1{Kitty and no other cats}
+		one{Kitty and # other cat}
+		other{Kitty and # other cats}}`,
+		"No Kitty",
+		0)
+
+	// real world example
+	pattern := `{0, select,
+  female {{
+      1, plural, offset:1
+      =0 {{2} does not give a party.}
+      =1 {{2} invites {3} to her party.}
+      =2 {{2} invites {3} and one other person to her party.}
+      other {{2} invites {3} and # other people to her party.}}}
+  male {{
+      1, plural, offset:1
+      =0 {{2} does not give a party.}
+      =1 {{2} invites {3} to his party.}
+      =2 {{2} invites {3} and one other person to his party.}
+      other {{2} invites {3} and # other people to his party.}}}
+  other {{
+      1, plural, offset:1
+      =0 {{2} does not give a party.}
+      =1 {{2} invites {3} to their party.}
+      =2 {{2} invites {3} and one other person to their party.}
+      other {{2} invites {3} and # other people to their party.}}}}`
+
+	gender := "female"
+	host := "Jane"
+	guest := "John"
+	test(pattern, "Jane does not give a party.", gender, 0, host, guest)
+	test(pattern, "Jane invites John to her party.", gender, 1, host, guest)
+	test(pattern, "Jane invites John and one other person to her party.", gender, 2, host, guest)
+	test(pattern, "Jane invites John and 2 other people to her party.", gender, 3, host, guest)
+
+	gender = "male"
+	host = "John"
+	guest = "Jane"
+	test(pattern, "John does not give a party.", gender, 0, host, guest)
+	test(pattern, "John invites Jane to his party.", gender, 1, host, guest)
+	test(pattern, "John invites Jane and one other person to his party.", gender, 2, host, guest)
+	test(pattern, "John invites Jane and 2 other people to his party.", gender, 3, host, guest)
+
+	gender = "unspecified"
+	host = "Sam"
+	guest = "Alex"
+	test(pattern, "Sam does not give a party.", gender, 0, host, guest)
+	test(pattern, "Sam invites Alex to their party.", gender, 1, host, guest)
+	test(pattern, "Sam invites Alex and one other person to their party.", gender, 2, host, guest)
+	test(pattern, "Sam invites Alex and 2 other people to their party.", gender, 3, host, guest)
 }
